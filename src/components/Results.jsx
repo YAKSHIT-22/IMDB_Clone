42ab373db859ffe3c712f76e1728715d10ddf99b
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-02-08 00:58:47 +0530

    Done

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Results({results}){
     return(
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-5xl xl:max-w-6xl mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-5xl xl:max-w-6xl mx-auto p-4">
             {results.map((result) => (
                 <Card key={result.id} result={result}/>
             ))}
